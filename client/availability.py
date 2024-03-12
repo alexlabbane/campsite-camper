bb93d2da7605d0fe59e76a5a576758a76c993c0b
alexlabbane <alex.labbane@gmail.com>
2024-03-12 06:39:43 +0000

    add TTL to data in Availability class

--- client/availability.py
+++ client/availability.py
@@ -1,5 +1,6 @@
 # Answer availability queries about the campsite
 import datetime
+from time import sleep
 from client import get_availability, RESPONSE_DATE_FORMAT
 
 # Check if specific site is available between start_date, end_date
@@ -17,40 +18,57 @@ def filter_site(campsite, start_date, end_date):
     return available
 
 class Availability:
+    DEFAULT_TTL_SEC = 60 * 5
+    DEFAULT_HTTP_RETRIES = 3
+    DEFAULT_RETRY_COOLDOWN_SEC = 3
+
     def __init__(self, campground_id, month=datetime.date.today().month, year=datetime.date.today().year):
         self.campground_id = campground_id
         self.month = month
         self.year = year
+        self.last_update = None
         self.refresh_data()
 
     # Re-fetch fresh data
     # TODO: Handle data that spans multiple months (smush multiple request together?)
-    def refresh_data(self):
+    # TTL is in seconds
+    def refresh_data(self, ttl=DEFAULT_TTL_SEC, retry=DEFAULT_HTTP_RETRIES):
+        cur_time = datetime.datetime.now()
+        if self.last_update is not None and (cur_time - datetime.timedelta(seconds=ttl)) < self.last_update:
+            return
+
         try:
             self.data = get_availability(self.campground_id, self.month, self.year)
             self.last_update = datetime.datetime.now()
         except:
-            print(f"{datetime.datetime.now()}: HTTP Request to recreation.gov failed")
+            print(f"{datetime.datetime.now()}: HTTP Request to recreation.gov failed. Retries left: {retry}")
+            if retry > 0:
+                print(f"Retrying in {DEFAULT_RETRY_COOLDOWN_SEC} seconds")
+                self.refresh_data(ttl, retry - 1)
             pass
 
     # Query functions
     def get_num_campsites(self):
+        self.refresh_data()
         return self.data["count"]
 
     # Return the filtered sites
     def filter_sites_by_capacity(self, num_people=1, campsites=None):
+        self.refresh_data()
         if campsites is None:
             campsites = self.data["campsites"]
 
         return {k:v for k,v in campsites.items() if v["min_num_people"] <= num_people and v["max_num_people"] >= num_people}
 
     def filter_sites_by_date(self, start_date, end_date, campsites=None):
+        self.refresh_data()
         if campsites is None:
             campsites = self.data["campsites"]
 
         return {k:v for k,v in campsites.items() if filter_site(v, start_date, end_date)}
 
     def filter_sites(self, start_date, end_date, num_people=1, campsites=None):
+        self.refresh_data()
         if campsites is None:
             campsites = self.data["campsites"]
         campsites = self.filter_sites_by_capacity(num_people, campsites)
@@ -66,3 +84,19 @@ if __name__=="__main__":
     start = datetime.date(2024, 4, 8)
     end = datetime.date(2024, 4, 8)
     print(availability.filter_sites(start, end, 4).keys())
+
+    print(availability.get_num_campsites())
+    sleep(1)
+    print(availability.get_num_campsites())
+    sleep(2)
+    print(availability.get_num_campsites())
+    sleep(2)
+    print(availability.get_num_campsites())
+    sleep(4)
+    print(availability.get_num_campsites())
+    sleep(1)
+    print(availability.get_num_campsites())
+    sleep(5)
+    print(availability.get_num_campsites())
+    sleep(5)
+    print(availability.get_num_campsites())
